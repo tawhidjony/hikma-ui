0c8d5f96de2fe979b44e5d90863336ae355980c6
Tawhid Jony <tawhid.jony.jhm@gmail.com>
2025-02-06 00:54:06 +0600

    feat(release.yml): configure github action

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,26 +1,27 @@
 import type { UserConfig } from '@commitlint/types';
 
-const Configuration:UserConfig = {
-    extends:["@commitlint/config-conventional"],
-    rules:  {
-        'type-enum':[
+const Configuration: UserConfig = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [
             2,
             'always',
             [
                 'feat',     // New features
-                'fix',      // Bug Fixing
-                'chore',    // Maintenance
-                'ci',       // CI Configurations changes
-                'docs',     // Documentions update
+                'fix',      // Bug fixes
+                'chore',    // Maintenance tasks
+                'ci',       // CI/CD configuration changes
+                'docs',     // Documentation updates
                 'perf',     // Performance improvements
-                'refactor', // Code Refactoring
-                'revert',   // Revert to previous commit
-                'style',    // Code Style change
-                'test',     // Adding or updated tests
-                
+                'refactor', // Code refactoring
+                'revert',   // Revert previous changes
+                'style',    // Code style/formatting changes
+                'test',     // Tests (add/update/remove)
+                'build',    // Build system changes
+                'deps'      // Dependencies updates
             ]
         ]
     }
-}
+};
 
-module.exports = Configuration;
+export default Configuration;
