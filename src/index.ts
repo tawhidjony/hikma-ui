8a310b3ae4ee134224c6f700cb1197082c329c1d
tawhidjony <tawhid.jony.jhm@gmail.com>
2025-02-05 18:28:52 +0600

    fix(export):fixing export default

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ export { default as UiDatepicker } from './components/UiBasicDatePicker/UiBasicD
 export { default as UiForm } from './components/UiForm/UiForm';
 export { default as UiButton } from './components/Button';
 export { default as UiGrid } from './components/Grid';
-export type { UiFormProps } from './components/UiForm/types/UiForm';
 export type { UiFormRefProps } from './components/UiForm/types/UiForm';
